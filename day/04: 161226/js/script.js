34873a95ad3021637c646759e6b87cade505e0e2
sirudduk <sirudduk@jungkyoonui-MacBook-Pro.local>
2016-12-26 20:02:21 +0900

    project 생성

---
+++ day/04: 161226/js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+    todo.init();
+
+
+});
+
+function Todo(){
+    this.init = function(){
+        var 
+        var $wrap = document.getElementById('wrap');
+        $wrap.appendChild
+    }
+}
+
+var todo = new Todo();
+
